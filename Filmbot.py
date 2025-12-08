4a28b40d1c7bdb4b566243a64dd1c82160837b82
Experience07 <beamerboy0707@gmail.com>
2025-12-08 16:28:11 +0300

    Update Filmbot.py

--- Filmbot.py
+++ Filmbot.py
@@ -1,4 +1,5 @@
 import random
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
@@ -9,19 +10,27 @@ films = [
     "Один дома",
     "Жмурки",
     "Бригада",
-    "Анжела перл расклад на год💅",
+    "Анжела Перл – расклад на год 💅",
     "Данила Поперечный: АГЕНТ 813"
 ]
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Напиши /film чтобы я выбрал фильм на сегодня 🎬")
+    await update.message.reply_text(
+        "Привет! Напиши /film чтобы я выбрал фильм на сегодня 🎬"
+    )
 
 async def film(update: Update, context: ContextTypes.DEFAULT_TYPE):
     movie = random.choice(films)
     await update.message.reply_text(f"Сегодня ты смотришь: 🎥 {movie}")
 
 def main():
-    app = ApplicationBuilder().token("8476573533:AAHr9iIMr1jw-wwJ9YOx3DfRL3tc1iNwGLQ").build()
+    # Получаем токен из переменной окружения на Render
+    TOKEN = os.getenv("BOT_TOKEN")
+
+    if TOKEN is None:
+        raise ValueError("❌ Ошибка: переменная окружения BOT_TOKEN не установлена!")
+
+    app = ApplicationBuilder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("film", film))
