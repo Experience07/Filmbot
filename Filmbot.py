8067c5da6afcdc0c762326b32b67d0ef2ba81ef8
Experience07 <beamerboy0707@gmail.com>
2025-12-08 16:21:54 +0300

    Add files via upload

---
+++ Filmbot.py
@@ -0,0 +1,32 @@
+import random
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+films = [
+    "Гарри Поттер",
+    "Слуга народа",
+    "Бумер",
+    "Один дома",
+    "Жмурки",
+    "Бригада",
+    "Анжела перл расклад на год💅",
+    "Данила Поперечный: АГЕНТ 813"
+]
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет! Напиши /film чтобы я выбрал фильм на сегодня 🎬")
+
+async def film(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    movie = random.choice(films)
+    await update.message.reply_text(f"Сегодня ты смотришь: 🎥 {movie}")
+
+def main():
+    app = ApplicationBuilder().token("8476573533:AAHr9iIMr1jw-wwJ9YOx3DfRL3tc1iNwGLQ").build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("film", film))
+
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
